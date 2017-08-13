b43cf2fd70d989ed5e6f2b2fb7d438aed87fa28b
GoldenPhysics <goldenphysicsroblox@gmail.com>
2017-08-13 13:45:08 -0500

    Added prefix and handling of prefix

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ console.log("client successfully created");
 //token for loging in
 const token = "MzQ2MzM1ODYyMTI2MDE4NTYx.DHIczA.6J2mpWP9X99QyAIVI57U2yjHcNc";
 
+//other program constants
+const prefix = "!";
+
 //action for when bot is ready
 client.on("ready", () => {
   console.log("Report: Ready");
@@ -19,6 +22,8 @@ client.on("ready", () => {
 
 //message handling
 client.on("message", msg => {
+  if (msg.content.startsWith() !== prefix) return;
+  msg.content = msg.content.substr(1);
   if (msg.content === "ping") {
     msg.reply("Pong!");
   }
