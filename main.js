ced4ccf5eb1a4eea347f59a9d855bd4e280dfc1c
GoldenPhysics <goldenphysicsroblox@gmail.com>
2017-08-13 13:40:12 -0500

    Added comments and removed ping timing. I think it was heartbeat timing?

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
-console.log("FirstTest");
+console.log("Starting bot");
+
+//require discord.js dependency
 const Discord = require("discord.js");
+console.log("discord.js successfully required")
 
+//create the client
 const client = new Discord.Client();
+console.log("client successfully created");
 
+//token for loging in
 const token = "MzQ2MzM1ODYyMTI2MDE4NTYx.DHIczA.6J2mpWP9X99QyAIVI57U2yjHcNc";
 
+//action for when bot is ready
 client.on("ready", () => {
   console.log("Report: Ready");
 
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong! Message time: ' + client.pings[0]);
-    console.log("ping time: " + client.ping);
+//message handling
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("Pong!");
   }
 });
 
+//login
 client.login(token);
