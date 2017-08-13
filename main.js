f7c3878bc6706bc58a32165d2ef4d9e0797b3931
GoldenPhysics <goldenphysicsroblox@gmail.com>
2017-08-13 13:25:35 -0500

    Trying to make ping time work.

--- main.js
+++ main.js
@@ -7,11 +7,15 @@ const token = "MzQ2MzM1ODYyMTI2MDE4NTYx.DHIczA.6J2mpWP9X99QyAIVI57U2yjHcNc";
 
 client.on("ready", () => {
   console.log("Report: Ready");
+
 });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong! Message time: ' + client.pings[0]);
+    for (let ping of client.pings) {
+      console.log("ping time is: " + ping);
+    }
   }
 });
 
