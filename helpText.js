fa5792179219656337d5d49e62e714ac40605e5a
GoldenPhysics <goldenphysicsroblox@gmail.com>
2017-08-14 11:13:55 -0500

    Added new lines.

--- helpText.js
+++ helpText.js
@@ -1,5 +1,5 @@
-const helpIntro = "This help command posts a list of commands.\
-    This list is not necessarily complete or acurate.\
+const helpIntro = "This help command posts a list of commands.\n\
+    This list is not necessarily complete or acurate.\n\
     Some commands may not yet be implemented.\n\n";
 
 blockQuote = function(str) {
@@ -8,16 +8,16 @@ blockQuote = function(str) {
 
 exports.help = function() {
   return blockQuote(helpIntro +
-      "Bot Admin commands:\
-      !help public (not implemented):\
-        displays this help text in the channel the request was made from.\
-      \
-      Bot User Commands:\
-      !help (not implemented):\
-        Sends the user this help text in a direct message\
-      !ping:\
-        Bot replys \"Pong!\" and the time it took to reply.\
-      !on-call:\
-        Toggles the on-call role for the command sender.\
-        Only works if the server has a role called \"On-call\".")
+      "Bot Admin commands:\n\
+      !help public (not implemented):\n\
+        \tdisplays this help text in the channel the request was made from.\n\
+      \n\
+      Bot User Commands:\n\
+      !help (not implemented):\n\
+        \tSends the user this help text in a direct message\n\
+      !ping:\n\
+        \tBot replys \"Pong!\" and the time it took to reply.\n\
+      !on-call:\n\
+        \tToggles the on-call role for the command sender.\n\
+        \tOnly works if the server has a role called \"On-call\".")
 }
